a6529f816ef2ec037ee5d8e6df2004cabd360cb8
Maria <mariaamoon98@gmail.com>
2025-04-21 18:20:57 +0330

    Card

--- src/Card.java
+++ src/Card.java
@@ -1,2 +1,11 @@
 public class Card {
+    private  String face;
+    private  String suit;
+    public Card(String cardface , String cardsuit){
+        this.face= cardface;
+        this.suit = cardsuit;
+    }
+    public String toString() {
+        return face+" "+suit;
+    }
 }
