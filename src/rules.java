cc0b66522699edb4eb4cca2d25f886b6be929085
mariaamoon008@gmail.com <mariaamoon98@gmail.com>
2025-04-22 13:30:54 +0330

    rule

---
+++ src/rules.java
@@ -0,0 +1,22 @@
+public class rules {
+    String khal;
+    enum face{
+        Deuce,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King,
+        Ace,
+        }
+        public void Khal(String kh){
+        khal = kh;
+        }
+    }
+
