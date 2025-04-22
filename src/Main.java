1713cede0cac3868d7fcd2a1833dcfeddc44b4c1
mariaamoon008@gmail.com <mariaamoon98@gmail.com>
2025-04-22 18:37:15 +0330

    main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        player player1 = new player("mari");
+        player1.addCard("10 of Hearts");
+        player1.showHand();
+        image tenofhearts = new image();
+        String playedCard = player1.playCard();
+        System.out.println(player1.getName()+"played: " + playedCard);
 
+        player1.showHand();
     }
 }
