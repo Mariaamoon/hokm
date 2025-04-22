e5cdb6117ec3cfc19025a7a40551a4af186bcba6
mariaamoon008@gmail.com <mariaamoon98@gmail.com>
2025-04-22 18:37:34 +0330

    player

---
+++ src/player.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class player {
+    private  String name;
+    private List<String> hand;
+
+    public player(String name) {
+        this.name = name;
+        this.hand = new ArrayList<>();
+    }
+    public  String getName() {
+        return name;
+    }
+
+
+    public void addCard(String card) {
+        hand.add(card);
+    }
+
+    public String playCard() {
+        if (!hand.isEmpty()) {
+            return hand.remove(0);
+        } else {
+            return null;
+        }
+    }
+
+    // Show all cards in hand
+    public List<String> getHand() {
+        return hand;
+    }
+
+    // Number of cards in hand
+    public int handSize() {
+        return hand.size();
+    }
+
+    // Display player's hand
+    public void showHand() {
+        System.out.println(name + "'s hand: " + hand);
+    }
+}
