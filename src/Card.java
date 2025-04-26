f104418b119092bac6a5a7ddf8792c9231bc1504
Maria <Mariaamoon98@gmail.com>
2025-04-26 22:07:12 +0330

    Card

--- src/Card.java
+++ src/Card.java
@@ -1,12 +1,59 @@
 public class Card {
     private String face;
     private String suit;
-
+    private String hokm;
     public Card(String cardface, String cardsuit) {
         this.face = cardface;
         this.suit = cardsuit;
     }
-    public String tostring(){
+public void sethokm(String h) {
+        this.hokm = h;
+}
+    public int setKhal(String kh){
+        int khal = 0;
+        if(kh.contains("hearts"))
+        khal = 1;
+        else if(kh.contains("diamonds"))
+        khal = 2;
+         else if(kh.contains("clubs"))
+             khal = 3;
+        else if (kh.contains("spades"))
+            khal = 4;
+        return khal;
+    }
+    public int setvalue(String s){
+        int value=0;
+        if(s.contains("Deuce"))
+            value = 2;
+        else if(s.contains("Three"))
+            value = 3;
+        else if(s.contains("Four"))
+            value = 4;
+        else if(s.contains("Five"))
+            value = 5;
+        else if(s.contains("Six"))
+            value = 6;
+        else if(s.contains("Seven"))
+            value = 7;
+        else if(s.contains("Eight"))
+            value = 8;
+        else if(s.contains("Nine"))
+            value = 9;
+        else if(s.contains("Ten"))
+            value = 10;
+        else if(s.contains("Jack"))
+            value = 11;
+        else if(s.contains("Queen"))
+            value = 12;
+        else if(s.contains("King"))
+            value = 13;
+        else if(s.contains("Ace"))
+            value = 14;
+        if(s.contains(hokm))
+            value += 20;
+        return value;
+    }
+    public String toString(){
         return face + " " + suit;
     }
 }
