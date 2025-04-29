2e5cdfd5dfe9b66312dae67eef54f5a0ca8dad9f
Maria <Mariaamoon98@gmail.com>
2025-04-29 06:56:10 +0330

    player

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        player player1 = new player("mari");
-        player1.addCard("10 of Hearts");
-        player1.showHand();
-        image tenofhearts = new image();
-        String playedCard = player1.playCard();
-        System.out.println(player1.getName()+"played: " + playedCard);
+        Deck deck = new Deck();
+        deck.shuffle();
+    player[] players = new player[3];
+        for (int i = 0; i < players.length; i++) {
+            players[i] = new player("mari " + (i + 1));
+        }
+deck.shuffle();
+deck.deal();
 
-        player1.showHand();
+players[0].addCard(deck.s);
+players[0].showHand();
+        deck.deal();
+        players[1].addCard(deck.s);
+        players[1].showHand();
+
+
+
     }
 }
