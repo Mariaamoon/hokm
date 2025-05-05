ef5e3e5def68de6a4fc1dbf26fb1106e192af738
Maria <Mariaamoon98@gmail.com>
2025-05-05 23:02:49 +0330

    player

--- src/player.java
+++ src/player.java
@@ -2,46 +2,57 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class player {
-    private  String name;
+    private String name;
     private List<String> hand;
 
     public player(String name) {
         this.name = name;
         this.hand = new ArrayList<>();
     }
-    public  String getName() {
-        return name;
-    }
 
+    public String getName() {
+        String nm = name;
+        return nm;
+    }
 
     public void addCard(String card) {
         hand.add(card);
     }
 
-    public String playCard() {
+    public String playCard(int m) {
         if (!hand.isEmpty()) {
-            return hand.remove(0);
+            return hand.remove(m);
         } else {
             return null;
         }
     }
 
-    // Show all cards in hand
-    public List<String> getHand() {
-        return hand;
+    public String getHand(int n) {
+        return hand.get(n);
     }
 
     public int handSize() {
         return hand.size();
     }
 
-    // Display player's hand
     public void showHand() {
         System.out.println(name + "'s hand: " + hand);
     }
+
     public boolean own(String a) {
         if (hand.contains(a))
             return true;
+        return false;
+    }
+
+    public boolean chooseCard(int a) {
+        for (String s : hand) {
+            if (s.contains(rules.hokm) && hand.get(a).contains(s)) {
+                return true;
+            } else if (s.contains(rules.hokm) == false)
+                return true;
+        }
+
         return false;
     }
 }
