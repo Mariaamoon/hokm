805031ae916a7bf16b223a055f885d9b7f5797e2
Maria <Mariaamoon98@gmail.com>
2025-05-05 23:00:51 +0330

    rules

--- src/rules.java
+++ src/rules.java
@@ -1,5 +1,34 @@
 public class rules {
+    public static String Hokm;
+    public static String hokm;
+    public static int team1;
+    public static int team2;
+    public static int wins1;
+    public static int wins2;
 
-
-
+    public static void setwins(int a) {
+        if (a % 2 == 0)
+            wins2++;
+        else
+            wins1++;
     }
+    public static boolean end() {
+        if(wins2 ==7 || wins1==7)
+            return true;
+        return false;
+    }
+    public void setHokm(String h) {
+        this.Hokm = h;
+    }
+
+    public void sethokm(String h) {
+        this.hokm = h;
+    }
+
+    public int winner() {
+        if (wins1 == 7)
+            return 1;
+        return 2;
+    }
+
+}
