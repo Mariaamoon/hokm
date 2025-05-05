d999374e6f2b9691e52e84e4f802a5d556d154c1
Maria <Mariaamoon98@gmail.com>
2025-05-05 23:01:36 +0330

    deck

--- src/Deck.java
+++ src/Deck.java
@@ -1,4 +1,3 @@
-
 import java.security.SecureRandom;
 import java.util.ArrayList;
 
@@ -6,43 +5,47 @@ public class Deck {
     public static String s;
     private Card[] deck;
     private int currentCard;
-    private static final int numberOfCards = 52 ;
+    private static final int numberOfCards = 52;
     private static final SecureRandom random = new SecureRandom();
+
     public Deck() {
         String[] suits = {"Clubs", "Diamonds", "Hearts", "Spades"};
-        String[] faces = {"Ace", "Deuce", "Three", "Four" ,"Five", "Six", "Seven", "Eight", "Nine","Ten","Jack","Queen","King"};
+        String[] faces = {"Ace", "Deuce", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"};
 
         deck = new Card[numberOfCards];
 
         currentCard = 0;
         for (int i = 0; i < deck.length; i++) {
-            deck[i] = new Card(faces[i%13], suits[i/13]);
-        }}
-        public void shuffle()
-    {
-            currentCard = 0;
-            for (int i = 0; i < deck.length; i++) {
-                int second = random.nextInt(numberOfCards);
-                Card temp = deck[i];
-                deck[i] = deck[second];
-                deck[second] = temp;
-            }
+            deck[i] = new Card(faces[i % 13], suits[i / 13]);
         }
-        public String toString(Card[] deck, int currentCard) {
-            return deck[currentCard].toString();
-        }
-        public Card deal(){
-            if(currentCard < deck.length || deck[currentCard] != null){
-                Card cardToDeal = deck[currentCard];
-                s=toString(deck, currentCard);
-                deck[currentCard] = null;
-                currentCard++;
-                return cardToDeal;
-            }
-                else
-                    return null;
-
-        }
-
     }
+
+    public void shuffle() {
+        currentCard = 0;
+        for (int i = 0; i < deck.length; i++) {
+            int second = random.nextInt(numberOfCards);
+            Card temp = deck[i];
+            deck[i] = deck[second];
+            deck[second] = temp;
+        }
+    }
+
+    public String toString(Card[] deck, int currentCard) {
+        return deck[currentCard].toString();
+    }
+
+    public Card deal() {
+        Card cardToDeal = deck[currentCard];
+        if (currentCard < deck.length || deck[currentCard] != null) {
+            s = toString(deck, currentCard);
+            deck[currentCard] = null;
+            currentCard++;
+            return cardToDeal;
+        }
+        while (deck[currentCard] == null) {
+            shuffle();
+        }
+        return cardToDeal;
+    }
+}
 
